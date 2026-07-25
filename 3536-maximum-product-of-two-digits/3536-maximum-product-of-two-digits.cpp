d6e7b1151584446d51ca9e4130058fed7dba8a4f
Ankit Wazir <ankitwazir30@gmail.com>
2026-07-26 01:10:45 +0530

    Time: 0 ms (100%), Space: 9.1 MB (29.76%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int> numbers;
+        while(n>0){
+            int digit= n%10;
+            n=n/10;
+            numbers.push_back(digit);
+
+        }
+        int a = 0 ; 
+        int b= 0;
+        for(int i = 0 ; i<numbers.size(); i++){
+               if(numbers[i]>a){
+                b=a;
+                a=numbers[i];
+               }
+               else if( b<numbers[i]){
+                b=numbers[i];
+               }
+               else{
+                continue;
+               }
+        }
+        return a*b;
+
+    }
+};
