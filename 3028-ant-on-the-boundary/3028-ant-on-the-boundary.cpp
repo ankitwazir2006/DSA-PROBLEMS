e6641391a8a9656f4596550549a0db4865acb532
Ankit Wazir <ankitwazir30@gmail.com>
2026-07-30 00:18:51 +0530

    Time: 3 ms (77.2%), Space: 23.5 MB (15.56%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int returnToBoundaryCount(vector<int>& nums) {
+        int sum = 0 ,ans=0;
+        for(int i = 0 ; i<nums.size();i++){
+             sum+=nums[i];
+             if(sum==0){
+                ans++;
+             }
+        }
+        return ans;
+    }
+};
