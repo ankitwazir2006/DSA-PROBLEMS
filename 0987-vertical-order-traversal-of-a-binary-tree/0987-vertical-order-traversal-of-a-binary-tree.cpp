a91b81bf35284d73e955941633d4135183cb0f70
Ankit Wazir <ankitwazir30@gmail.com>
2026-07-31 23:31:21 +0530

    Time: 0 ms (100%), Space: 15.9 MB (64.84%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,41 +12,40 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,vector<int>>>nodes;
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        vector<vector<int>>ans;
-        if (root==NULL){
+        map<int,map<int,vector<int>>> mp;
+        vector<vector<int>> ans;
+        if(root==NULL){
             return ans;
         }
-        q.push(make_pair(root,make_pair(0,0)));
-        
+        queue <pair<TreeNode*,pair<int,int>>> q; // ist int is horizontal distance , 2nd int is its level 
+        q.push(make_pair(root, make_pair(0,0)));
         while(!q.empty()){
-            pair<TreeNode*,pair<int,int>>temp=q.front();
+            pair<TreeNode*,pair<int,int>> temp=q.front();
             q.pop();
-            TreeNode* frontnode=temp.first;
             int hd=temp.second.first;
-            int level=temp.second.second;
-            
-            nodes[hd][level].push_back(frontnode->val);
+            int level= temp.second.second;
+            TreeNode*frontnode= temp.first;
+            mp[hd][level].push_back(frontnode->val);
+
             if(frontnode->left){
                 q.push(make_pair(frontnode->left,make_pair(hd-1,level+1)));
             }
-            if (frontnode->right){
+            if(frontnode->right){
                 q.push(make_pair(frontnode->right,make_pair(hd+1,level+1)));
             }
-            
+
         }
-        for(auto i:nodes){
-            // i <int,map>
-            vector<int> temp;
-            for( auto j:i.second){
+        
+        for(auto i: mp){
+             vector<int> temp;
+             //i==<TreeNode*,map<int,vector<int>>>
+             for(auto j: i.second ){
                 sort(j.second.begin(),j.second.end());
-                //j <int,vector<int>>
-              for(int val : j.second){
-                  temp.push_back(val);
-              }
-            }
-            ans.push_back(temp);
+                for(int k : j.second){
+                    temp.push_back(k);
+                }
+             }
+             ans.push_back(temp);
         }
         return ans;
     }
