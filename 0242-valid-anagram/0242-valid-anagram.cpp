45c784ff66662d753fb83135f059391f3c470d29
Ankit Wazir <ankitwazir30@gmail.com>
2026-08-13 14:38:04 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,19 +4,17 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-         unordered_map<char,int> mp1;
-         unordered_map<char,int>mp2;
-         for(int i = 0 ; i< s.size();i++){
-            char c1= s[i];
-            char c2=t[i];
-            mp1[c1]++;
-            mp2[c2]++;
-         }
-         for(int i = 0 ; i<s.size();i++){
-            if(mp1[s[i]]!=mp2[s[i]]){
+        int arr[26];
+        int arr2[26];
+        for(int i= 0 ; i<s.size();i++){
+            arr[s[i]-'a']++;
+            arr2[t[i]-'a']++;
+        }
+        for(int i = 0 ; i< s.size();i++){
+            if(arr[s[i]-'a']!=arr2[s[i]-'a']){
                 return false;
             }
-         }
-         return true;
+        }
+        return true;
     }
 };
